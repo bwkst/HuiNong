a74e07fc780412a70d6ea0c4f6d5799ecdcbb18b
bwkst <s.t.kristine.or@gmail.com>
2022-05-25 18:04:56 +0800

    授权用户个人信息完成
    Future Development: Check whether update is needed. Coz wx.getUserInfo has changed to wx.getUserProfile

--- miniprogram/pages/authorize/authorize.js
+++ miniprogram/pages/authorize/authorize.js
@@ -1,86 +1,57 @@
-// pages/authorize/authorize.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    envId: '',
-    openId: ''
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    this.setData({
-      envId: options.envId
-    });
-    this.getOpenId();
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  },
-
-  getOpenId() {
-    wx.cloud.callFunction({
-       name: 'quickstartFunctions',
-       config: {
-         env: this.data.envId
-       },
-       data: {
-         type: 'getOpenId'
-       }
-     }).then((res) => {
-       this.setData({
-         openId: res.result.openid
-       });
+  next: function(e) {
+    console.log("userInfo", getApp().globalData.userInfo)
+    wx.redirectTo({
+      url: '/pages/login/login'
     })
-   }
+  },
+
+  onLoad: function(options) {
+    var that = this
+
+    wx.showLoading({
+      title: '加载中',
+    })
+
+    wx.login({
+      success(res) {
+        if (res.code) {
+        
+          // 查看是否授权
+          wx.getSetting({
+            success(res) {
+              if (res.authSetting['scope.userInfo']) {
+                // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+                wx.getUserInfo({
+                  success: function(res) {
+                    getApp().globalData.userInfo = res.userInfo
+                    that.next();
+                  }
+                })
+              }
+            }
+          })
+
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+
+
+    setTimeout(function() {
+      wx.hideLoading()
+    }, 2000)
+  },
+
+  bindGetUserInfo(e) {
+    getApp().globalData.userInfo = e.detail.userInfo
+    wx.redirectTo({
+      url: '/pages/authorize/authorize'
+    })
+  }
 })
