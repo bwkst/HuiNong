ae525c8d44ff78930d7599c1ac8e807781dadc26
2511253367 <2511253367@qq.com>
2022-05-27 22:47:22 +0800

    更新了页面场景，和部分js逻辑。调用云数据的部分还没弄

--- miniprogram/pages/resetPassword/resetPassword.js
+++ miniprogram/pages/resetPassword/resetPassword.js
@@ -5,13 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    shouji1:0,
-    shenfen1:0,
-    mima11:0,
-    mima21:0,
-    judge1:false,
-    judge2:false,
-    judge3:true,
+    shouji1: 0,//存放手机号码
+    shenfen1: 0,//存放身份证后四位
+    mima11: 0,//存放一号密码框
+    mima21: 0,//存放二号密码框
+    judge1: true,//表示手机号码是否就绪
+    judge2: true,//判断身份证号码是否正确
+    judge3: true,//判断两次密码是否相同
+    jingao1: '',//密码不同的警告文字
+    tishiyu1: '',//提示身份证号码输入错误
   },
 
   /**
@@ -34,33 +36,60 @@ Page({
   onShow() {
 
   },
-shouji(e){
-  console.log(e.detail);
-  this.data.shouji1=e.detail
-},
-
-shenfen(e){
-  console.log(e.detail);
-  this.data.shenfen1=e.detail
-},
-mima1(e){
-  var that=this;
-  this.data.mima11=e.detail;
-  console.log(this.data.mima11);
-  if(this.data.mima21==this.data.mima11){
-    that.setData({judge1:false})}
-    else{that.setData({judge1:true})}
-},
-mima2(e){
-  var that=this;
-  console.log(e.detail);
-  this.data.mima21=e.detail;
-  if(this.data.mima21==this.data.mima11){
-  that.setData({judge1:false})}
-  else{that.setData({judge1:true})}
-},
-sendpasswords(){
-
+  //录入手机号的函数
+  shouji(e) {
+    this.data.shouji1 = e.detail["value"];
+    console.log(e.detail);
+    if (e.detail["cursor"] == 11) {
+      this.judge1 = true;
+      //上传手机号到云端
+    }
+    else {
+      this.judge1 = false;
+    }
+  },
+  //判断身份证号码的函数
+  shenfen(e) {
+    this.data.shenfen1 = e.detail["value"];
+    if (judge1 && e.detail["cursor"] == 4) {
+      //从云端获取正确的身份证号码并比对
+      //if(输入的身份证号和云端相同)
+      //this.judge2=ture;
+      //this.setData({tishiyu1:'身份号码正确'})
+      //else
+      //this.judge2=false;
+      //this.setData({tishiyu1:'身份号码错误'})
+    }
+    else
+      this.setData({ tishiyu1: ' ' });
+  },
+  //录入密码的函数
+  mima1(e) {
+    var that = this;
+    this.data.mima11 = e.detail["value"];
+    console.log(this.data.mima11);
+    if (this.data.mima21 == this.data.mima11) {
+      that.setData({ jinggao1: '' })
+    }
+    else { that.setData({ jinggao1: "两次输入的密码不同" }) }
+  },
+  //确认密码的函数
+  mima2(e) {
+    var that = this;
+    this.data.mima21 = e.detail["value"];
+    if (this.data.mima21 == this.data.mima11) {
+      that.setData({ jinggao1: '' })
+    }
+    else { that.setData({ jinggao1: "两次输入的密码不同" }) }
+  },
+  //确认修改之后的函数
+  sendpasswords() {
+    if (judge1&&judge2&&judge3){
+      //把重设的密码发送到云端
+      wx.redirectTo({
+        url: 'pages/register/register',//跳转到登录界面
+      })
+    }
 },
 
   /**
