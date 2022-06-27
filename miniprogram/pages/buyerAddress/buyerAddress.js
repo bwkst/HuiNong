177a33afc32707b5bca4e0a6529f0b85320e429f
bwkst <s.t.kristine.or@gmail.com>
2022-06-27 12:43:08 +0800

    下单 & 修改地址页面（前端完成）
    下单页面选用了跳转到修改地址页面作修改
    修改地址页面返回上一级（下单/个人中心）

--- miniprogram/pages/buyerAddress/buyerAddress.js
+++ miniprogram/pages/buyerAddress/buyerAddress.js
@@ -1,78 +1,100 @@
 // pages/buyerAddress/buyerAddress.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    nickName: "微信昵称",
-    phoneNo: "11111111111",
-    address: "北京市昌平区",
-    status: "我的订单",
-    List: 7,//实例展示
-    zhuangtai: "weijichu",
-
+    deliveryName: "",
+    deliveryPhoneNo: "",
+    deliveryRegion: "",
+    deliverySheng: "",
+    deliveryShi: "",
+    deliveryQu: "",
+    deliveryAddress: "",
+    customItem: '全部',
+    status: "显示",
   },
 
-  return: function (e) {
-    wx.navigateTo({
-      url: '/pages/buyerCenter/buyerCenter',
+  onLoad: function () {
+    this.setData({            //从数据库拉下来
+      deliveryName: "未输入收货人",
+      deliveryPhoneNo: "未输入收货手机号",
+      deliveryRegion: "未输入收货地区",
+      deliverySheng: "未输入收货省",
+      deliveryShi: "未输入收货市",
+      deliveryQu: "未输入收货区",
+      deliveryAddress: "未输入收货详细地址",
+      customItem: '全部',
     })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  bindRegionChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      deliveryRegion: e.detail.value,
+      deliverySheng: e.detail.value[0],
+      deliveryShi: e.detail.value[1],
+      deliveryQu: e.detail.value[2]
+    })
+    this.haveChange();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  haveChange: function () {           //目前的修改数据是否和数据库内数据对比，有区别才可以提交修改
+    //下方condition需改
+    if (DatabaseName != this.data.deliveryName) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else if (DatabasePhoneNo != this.data.deliveryPhoneNo) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else if (DatabaseRegion != this.data.deliveryRegion) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else if (DatabaseSheng != this.data.deliverySheng) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else if (DatabaseShi != this.data.deliveryShi) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else if (DatabaseQu != this.data.deliveryQu) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else if (DatabaseAddress != this.data.deliveryAddress) {
+      this.setData({
+        status: "修改完成"
+      })
+    } else {
+      this.setData({
+        status: "修改中"
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  return: function (e) {
+    wx.navigateBack({
+      delta: 1
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  changeInfo: function () {
+    this.setData({
+      status: "修改中"
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  submitChange: function () {
+    this.setData({
+      status: "修改完成"
+    })
+    //更新数据库
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  cancelChange: function () {
+    this.setData({
+      status: "显示"
+    })
   }
 })
--- miniprogram/pages/buyerOrder/buyerOrder.js
+++ miniprogram/pages/buyerOrder/buyerOrder.js
@@ -1,12 +1,24 @@
 // pages/buyerOrder/buyerOrder.js
 Page({
   data: {
+    deliveryName: "",
+    deliveryPhoneNo: "",
+    deliveryRegion: "",
+    deliverySheng: "",
+    deliveryShi: "",
+    deliveryQu: "",
+    deliveryAddress: "",
   },
 
   centerPage: function(){
     wx.redirectTo({
       url: '../buyerCenter/buyerCenter',
     })
-  }
+  },
 
+  buyerAddress: function(){
+    wx.navigateTo({
+      url: '../buyerAddress/buyerAddress',
+    })
+  }
 })
