1e9e3f6bb41d956ce80b3898ae121184bb5843f1
2511253367 <2511253367@qq.com>
2022-06-27 14:49:22 +0800

    增加了数据库一的相关操作
    目前可以上传订单数据

--- miniprogram/pages/fabushangpin/fabushangpin.js
+++ miniprogram/pages/fabushangpin/fabushangpin.js
@@ -10,8 +10,9 @@ Page({
     price:0,
     address:['','',''],
     time:'',
-    pictureaddress:"",
     number:0,
+    photopath:'',
+    photo1:false,//判断是否上传图片
     judge:false//判断信息是否填写完整
   },
 
@@ -29,51 +30,107 @@ Page({
       console.log(this.data.judge)
     },
 
-  //发布产品的函数
-  post:function(){
-
-  },
-
   //输入产品名称
   chanpinmingcheng:function(e){
     this.data.goodsname = e.detail.value;
-  },
-
-  //输入产品价格
-  chanpinjiage:function(e){
-    this.data.price = e.detail.value;
+    this.judge1();
+    console.log('名称');
   },
 
   //输入产品数量
   chanpinshuliang:function(e){
-    this.data.amount = e.detail.value;
+   this.data.amount = e.detail.value;
+   this.judge1();
+   console.log('名称');
+ },
+
+  //输入产品价格
+  chanpinjiage:function(e){
+    this.data.price = e.detail.value;
+    this.judge1();
+    console.log('名称');
   },
 
   //选择地址
   addressinput: function (e) {
-    console.log(e);
     this.setData({
       ['address[0]']:e.detail.value[0],
       ['address[1]']:e.detail.value[1],
       ['address[2]']:e.detail.value[2],
     })
-    console.log(this.data.address)
+    this.judge1();
+    console.log('名称');
   },
 
   //选择时间
   timeinput:function(e){
-    console.log(e);
     this.setData({
       time:e.detail.value
     })
+    this.judge1();
+    console.log('名称');
   },
 
   //输入联系电话
   lianxidianhua:function(e){
     this.data.number = e.detail.value;
-    this.judge1;
-    console.log('联系电话正常')
+    this.judge1();
+    console.log('联系电话正常');
   },
+
+  //获取照片本地地址
+  picture1:function(){
+    wx.chooseImage({
+      count:1,
+      success:res=>{
+        this.data.photopath=res.tempFilePaths[0];
+        console.log(this.data.photopath);
+      }
+    })
+    this.data.photo1=true;
+    console.log('上传照片函数');
+    console.log(this.data.photopath);
+    console.log(this.data.photo1);
+  },
+
+    //确认发布的函数
+    post:function(){
+      this.judge1();
+      //上传照片到数据库
+      if(this.data.photo1==true){
+          wx.cloud.uploadFile({
+          filePath:this.data.photopath,
+          name: this.data.number+Date.now(),
+          cloudPath:"orderform/photo"+this.data.number+Date.now()+'.jpg'
+        })
+        .then(res=>{
+          console.log('照片上传成功')
+        })
+      }
+      //上传其他信息到数据库
+      wx.cloud.database().collection('orderform')
+      .add({
+        data:{
+          goodsname:this.data.goodsname,
+          amount:this.data.amount,
+          price:this.data.price,
+          address:[this.data.address[0],this.data.address[1],this.data.address[2]],
+          time:this.data.time,
+          number:this.data.number,
+          identify:Date.now()
+        }
+      })
+      .then(res=>{ 
+        console.log('添加成功')
+      })
+      .catch(err=>{
+        console.log('添加失败',err)
+      })
+      //跳转到卖家个人中心
+      wx.redirectTo({
+        url: '/pages/sellerCenter/sellerCenter'     //跳转到卖家个人中心界面
+      })
+    },
 
   /**
    * 生命周期函数--监听页面加载
