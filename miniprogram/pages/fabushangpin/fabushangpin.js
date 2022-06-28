a4df3e670cd38c60663fbd47e283570e82854e76
2511253367 <2511253367@qq.com>
2022-06-28 11:00:14 +0800

    修改图片索引。删除无关变量

--- miniprogram/pages/fabushangpin/fabushangpin.js
+++ miniprogram/pages/fabushangpin/fabushangpin.js
@@ -7,16 +7,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-    goodsname:"",
-    amount:0,
-    price:0,
-    address:['','',''],
-    time:'',
-    number:'',
-    photopath:'../../img/loading.png',
-    cloudphotopath:'',
+    goodsname:"",//产品名称
+    amount:0,//数量
+    price:0,//价格
+    address:['','',''],//产品发货地址
+    time:'',//产品出产时间
+    number:'',//用户电话号码
+    photopath:'../../img/loading.png',//小程序的临时文件路径
+    photoID:'',//图片的唯一标识符
     photo1:false,//判断是否上传图片
-    judge:false//判断信息是否填写完整
+    judge:false,//判断信息是否填写完整
+    numbertext:'点击获取手机号',//按钮一的文字
+    phototext:'点击上传图片',//按钮二的文字
   },
 
     //判断信息是否输入完毕
@@ -41,7 +43,8 @@ Page({
       // res.data 包含该记录的数据
       console.log(res.data)
       this.setData({
-        number:res.data.phoneNumber
+        number:res.data.phoneNumber,
+        numbertext:'手机号已获取',
       })
       this.judge1();
       wx.showModal({
@@ -59,7 +62,8 @@ Page({
      count:1,
      success:res=>{
        this.setData({
-         photopath:res.tempFilePaths[0]
+         photopath:res.tempFilePaths[0],
+         phototext:'点击重新上传',
        })
        console.log(this.data.photopath);
      }
@@ -113,37 +117,40 @@ Page({
 
     //确认发布的函数
     post:function(){
-      this.judge1();
       //上传照片到数据库
       if(this.data.photo1==true){
           wx.cloud.uploadFile({
           filePath:this.data.photopath,
           name: this.data.number+Date.now(),
-          cloudPath:"orderform/photo"+this.data.number+Date.now()+'.jpg'
-        })
-        .then(res=>{
-          console.log('照片上传成功')
+          cloudPath:"orderform/photo/"+this.data.number+Date.now()+'.jpg',
+          success:res=>{
+            console.log('下面是上传图片的id');
+            console.log(res.fileID);
+            this.setData({
+              photoID:res.fileID
+            })
+            console.log(this.data.photoID)
+            //上传其他信息到数据库
+            wx.cloud.database().collection('orderform')
+            .add({
+              data:{
+                goodsname:this.data.goodsname,
+                amount:this.data.amount,
+                price:this.data.price,
+                address:[this.data.address[0],this.data.address[1],this.data.address[2]],
+                time:this.data.time,
+                number:this.data.number,
+                photoID:this.data.photoID,
+              },
+              success:function(res){
+                console.log("下面是上传函数之后的云图片地址")
+                concole.log('1')
+              }
+            })
+            //上传信息完毕
+          },
         })
       }
-      //上传其他信息到数据库
-      wx.cloud.database().collection('orderform')
-      .add({
-        data:{
-          goodsname:this.data.goodsname,
-          amount:this.data.amount,
-          price:this.data.price,
-          address:[this.data.address[0],this.data.address[1],this.data.address[2]],
-          time:this.data.time,
-          number:this.data.number,
-          identify:Date.now()
-        }
-      })
-      .then(res=>{ 
-        console.log('添加成功')
-      })
-      .catch(err=>{
-        console.log('添加失败',err)
-      })
       //跳转到卖家个人中心
       wx.redirectTo({
         url: '/pages/sellerCenter/sellerCenter'     //跳转到卖家个人中心界面
