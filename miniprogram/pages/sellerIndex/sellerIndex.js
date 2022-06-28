0ead549ba4da7ed7dc4c0b39d1a8f8da7d2c9e32
wzilu <2625683380@qq.com>
2022-06-28 20:53:13 +0800

    Revert "Revert "按时间排序，卖家中心做了一部分""
    This reverts commit 4421287bf69aef9f08a521f3dc427a844d3b3490.

--- miniprogram/pages/sellerIndex/sellerIndex.js
+++ miniprogram/pages/sellerIndex/sellerIndex.js
@@ -1,14 +1,73 @@
-// pages/sellerIndex/sellerIndex.js
 Page({
   data: {
-    zhuangtai: "weijichu",
-    List: 7,          //示例展示
+    datalist: [],
   },
 
-  onLoad() {
+  getData(num=5,page=0) {
+    wx.cloud.callFunction({
+      name: "sellercenter",
+      data:{
+        num:num,
+        page:page
+      }
+    }).then(res => {
+      var oldData=this.data.datalist
+      var newData=oldData.concat(res.result.data);
+      console.log(res.result.data)
+      this.setData({
+        datalist: newData
+      })
+    })
+  },
+  
+  onLoad: function (options) {
+    this.getData();
+  },
+// 触底
+  onReachBottom:function(){
+    var page=this.data.datalist.length
+  this.getData(5,page)//5为每次刷新的次数
   },
 
-  sellerCenterPage: function () {
+  //下单的函数
+  clickToOrder: function (e) {
+    var orderId = e.currentTarget.id;
+    getApp().globalData.orderCloudId = orderId;
+    console.log(getApp().globalData.orderCloudId);
+    wx.navigateTo({
+      url: "../buyerOrder/buyerOrder",
+    })
+  },
+
+  //获取卖家联系方式的函数
+  clickToGetContact: function (e) {
+    var sellPhoneNum = e.currentTarget.id;
+    console.log(e.currentTarget)
+    wx.showModal({
+      title: '卖家的联系方式',
+      content: sellPhoneNum,
+      showCancel: true,
+      success(res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+          wx.setClipboardData({
+            data: sellPhoneNum,   //云数据库中该订单的卖家手机号
+            success: (res) => {
+              wx.showModal({
+                title: '卖家信息已复制到剪贴板中',
+                content: '',
+                showCancel: false,
+              })
+            }
+          })
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
+
+  buyerCenterPage: function () {
     wx.redirectTo({
       url: '../sellerCenter/sellerCenter',
     })
